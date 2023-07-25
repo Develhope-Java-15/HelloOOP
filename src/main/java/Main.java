56a4aab8cb4b3061511b5014380787e017b7bac0
aegroto <aegroto@protonmail.com>
2023-07-25 14:27:02 +0200

    first example with Person class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Person baby = new Person("Mario", "Rossi");
+        Person elder = new Person("Gioacchino", "Rossi", LocalDate.of(1940, 10, 10));
+
+        System.out.println(baby.getFullName());
+        System.out.println(elder.getFullName());
     }
 }
---
+++ src/main/java/Person.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+
+    public Person(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Person(String firstName, String lastName) {
+        this(firstName, lastName, LocalDate.now());
+    }
+
+    public String getFirstName() {
+       return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
